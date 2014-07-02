52bcbab587f341d33b26043fbd8b4253e38f2b5e
yangdy <yangdy@outlook.com>
2014-07-02 13:49:47 +0800

    Tutorial 5: Relationships & Hyperlinked APIs

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,17 +3,17 @@ from rest_framework import serializers
 from .models import Snippet
 from django.contrib.auth.models import User
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
   owner = serializers.Field(source='owner.username')
-  
+
   class Meta:
     model = Snippet
-    fields = ('id', 'title', 'code', 'linenos', 'owner')
+    fields = ('url', 'title', 'code', 'linenos', 'owner')
 
-class UserSerializer(serializers.ModelSerializer):
-  snippets = serializers.PrimaryKeyRelatedField(many=True)
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+  snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail')
 
   class Meta:
     model = User
-    fields = ('id', 'username', 'snippets')
+    fields = ('url', 'username', 'snippets')
 
--- snippets/views.py
+++ snippets/views.py
@@ -5,6 +5,18 @@ from rest_framework import generics
 from rest_framework import permissions
 from django.contrib.auth.models import User
 
+from rest_framework import renderers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+
+@api_view(('GET',))
+def api_root(request, format=None):
+  return Response({
+    'users': reverse('user-list', request=request, format=format),
+    'snippets': reverse('snippet-list', request=request, format=format),
+  })
+
 class SnippetList(generics.ListCreateAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
